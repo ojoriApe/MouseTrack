2717435e49225750ee8188f864959743d088f08f
ojoriApe <67466049+ojoriApe@users.noreply.github.com>
2020-07-10 22:30:52 +0900

    0.1

---
+++ MouseTrack/Assets/Script/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D bulletRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletRigidbody = GetComponent<Rigidbody2D>();
+        bulletRigidbody.velocity = transform.forward * speed;
+
+        Destroy(gameObject, 5f);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            MouseTrack player = collision.GetComponent<MouseTrack>();
+
+            if (player != null)
+            {
+                player.Die();
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ MouseTrack/Assets/Script/MouseTrack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MouseTrack : MonoBehaviour
+{
+    
+    private Rigidbody2D playerRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newPosition = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+        playerRigidbody.transform.position = new Vector2(newPosition.x, newPosition.y);
+        
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
